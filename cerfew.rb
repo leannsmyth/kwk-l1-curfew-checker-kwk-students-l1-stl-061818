4a5ff3e6e5cd9a84148cd3d746e96f3fcd2d91d9
leannsmyth <leannmsmyth@gmail.com>
2018-06-19 18:12:04 +0000

    automatically backed up by learn

--- cerfew.rb
+++ cerfew.rb
@@ -1,9 +1,5 @@
 if Time.now.hour >=23
     puts "go home"
-elsif Time.now.hour >=11 && Time.now.hour <= 17
-    puts "good afternoon"
-elsif Time.now.hour >=17 && Time.now.hour <= 20
-    puts "good night"
 else
-    puts "late"
+    puts "ya got time"
 end
